de63bdad41eee2ae05652a04dbeb886971248c02
kriselaskenderi <37930129+kriselaskenderi@users.noreply.github.com>
2019-04-09 18:37:46 +0300

    Changes to Player

--- src/gazillion/QPlayer.java
+++ src/gazillion/QPlayer.java
@@ -1,27 +1,38 @@
 package gazillion;
 
+import logic.*;
+
 /**
- * QPlayer: PURELY TEST
  *
- * @author Unsal Ozturk
- * @version 20190328
+ * @author
+ * @version
  */
+
 public class QPlayer {
+
+    private int initialHealth = 5;
+    private int initialHints= 5;
+    private int initialCoins = 20;
+    private int initialTimeUp = 5;
+
     private String name;
     private int noHints;
     private int noHealth;
+    private int noHealthPowerUp;
     private int noCoins;
     private int noTimeUp;
 
-    public QPlayer() {
-        noHints = 0;
-        noCoins = 221;
-        noHealth = 3;
-        noTimeUp = 5;
-        name = "John Doe";
 
+    public QPlayer( String plname) {
+        name = plname;                      // maaybe might check later for stored players
+        noHints = initialHints;
+        noCoins = initialCoins;
+        noHealth = initialHealth;
+        noTimeUp = initialTimeUp;
+        noHealthPowerUp = 0;
     }
 
+    ///////////////////////////////////////////// GETTERS //////////////////////////////////////////////////
     public String getName() {
         return name;
     }
@@ -38,23 +49,34 @@ public class QPlayer {
         return noCoins;
     }
 
+    public int getNoHealthPowerUp() {
+        return noHealthPowerUp;
+    }
+
     public int getNoTimeUp() {
         return noTimeUp;
     }
 
-    public void setNoHints(int noHints) {
-        this.noHints = noHints;
-    }
+    ///////////////////////////////////////////// SETTERS //////////////////////////////////////////////////
 
-    public void setNoHealth(int noHealth) {
-        this.noHealth = noHealth;
-    }
+    public void setNoHints( int newNo) { noHints = newNo; }
 
-    public void setNoCoins(int noCoins) {
-        this.noCoins = noCoins;
-    }
+    public void  setNoTimeUp( int newNo ) { noTimeUp = newNo; }
 
-    public void setNoTimeUp(int noTimeUp) {
-        this.noTimeUp = noTimeUp;
+    public void setNoHealthPowerUp( int newNo) { noHealthPowerUp = newNo; }
+
+    public void setHealth( int newNo){ noHealth = newNo;}
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void unpackAward( QAward award) {
+
+        if (award != null){
+            // does not store info about collected pieces in Treasure mode
+            noHealthPowerUp = Math.max(0, noHealth + award.getHealthAwardNo());
+            noHints = Math.max(0, noHints + award.getHintsAwardNo());
+            noCoins = Math.max(0, noCoins + award.getCoinsAwardNo());
+            noTimeUp = Math.max(0, noTimeUp + award.getTimeAwardNo());
+        }
     }
 }
