69e66763df82e99e9d9e73c27e43622015846cc9
Ana Pecini <anna.pecini@yahoo.com>
2019-05-07 23:17:08 +0300

    Revert "Add files via upload"
    This reverts commit c8820f0068f0c561a88753b9e9295b291449f876.

--- src/src/utils/Observable.java
+++
@@ -1,6 +0,0 @@
-package utils;
-
-public interface Observable {
-    public void addObserver(Observer o);
-    public void notifyObservers();
-}
--- src/src/utils/Observer.java
+++
@@ -1,5 +0,0 @@
-package utils;
-
-public interface Observer {
-    public void update(Message msg);
-}
